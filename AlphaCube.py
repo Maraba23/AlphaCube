dc9a17c499b4e25e4800b3bfd708eaea7194f36f
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-03-30 15:57:32 -0300

    Update AlphaCube.py

--- AlphaCube.py
+++ AlphaCube.py
@@ -1,105 +1,82 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pygame
-import time
 import math
-import random
 
-############## COLORS ##############
+# define the size of the cube and the focal length
+cube_size = 200
+focal_length = 500
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+# define the corners of the cube relative to its center
+half_cube = cube_size // 2
+corners = [(-half_cube, -half_cube, half_cube),
+           (half_cube, -half_cube, half_cube),
+           (half_cube, half_cube, half_cube),
+           (-half_cube, half_cube, half_cube),
+           (-half_cube, -half_cube, -half_cube),
+           (half_cube, -half_cube, -half_cube),
+           (half_cube, half_cube, -half_cube),
+           (-half_cube, half_cube, -half_cube)]
 
-####################################
+# initialize Pygame and create a window
+pygame.init()
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Rotating Cube")
 
-############## Cube ##############
-pontos = []
+# define a function to draw the cube with a given rotation angle
+def draw_cube(angle):
+    positions = []
 
-pontos.append(np.matrix([-1, -1, 1]))
-pontos.append(np.matrix([1, -1, 1]))
-pontos.append(np.matrix([1,  1, 1]))
-pontos.append(np.matrix([-1, 1, 1]))
-pontos.append(np.matrix([-1, -1, -1]))
-pontos.append(np.matrix([1, -1, -1]))
-pontos.append(np.matrix([1, 1, -1]))
-pontos.append(np.matrix([-1, 1, -1]))
+    # apply the rotation transform to each corner
+    cos_angle = math.cos(angle)
+    sin_angle = math.sin(angle)
+    for x, y, z in corners:
+        # rotate around the Y axis (vertical)
+        rotated_x = x * cos_angle + z * sin_angle
+        rotated_z = z * cos_angle - x * sin_angle
+        # rotate around the X axis (horizontal)
+        rotated_y = y * cos_angle + rotated_z * sin_angle
+        rotated_z = rotated_z * cos_angle - y * sin_angle
+        # calculate the apparent position based on the focal length
+        app_x = int(rotated_x * focal_length / (rotated_z + focal_length) + width / 2)
+        app_y = int(rotated_y * focal_length / (rotated_z + focal_length) + height / 2)
+        positions.append((app_x, app_y))
 
-matriz_de_projecao = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 0]])
+    # draw the edges of the cube using the rotated positions
+    pygame.draw.line(screen, (255, 255, 255), positions[0], positions[1])
+    pygame.draw.line(screen, (255, 255, 255), positions[1], positions[2])
+    pygame.draw.line(screen, (255, 255, 255), positions[2], positions[3])
+    pygame.draw.line(screen, (255, 255, 255), positions[3], positions[0])
+    pygame.draw.line(screen, (255, 255, 255), positions[4], positions[5])
+    pygame.draw.line(screen, (255, 255, 255), positions[5], positions[6])
+    pygame.draw.line(screen, (255, 255, 255), positions[6], positions[7])
+    pygame.draw.line(screen, (255, 255, 255), positions[7], positions[4])
+    pygame.draw.line(screen, (255, 255, 255), positions[0], positions[4])
+    pygame.draw.line(screen, (255, 255, 255), positions[1], positions[5])
+    pygame.draw.line(screen, (255, 255, 255), positions[2], positions[6])
+    pygame.draw.line(screen, (255, 255, 255), positions[3], positions[7])
 
-angulo = 0
+# initialize the rotation angle to zero
+angle = 0
 
-pontos_2d = [[n,n] for n in range(len(pontos))]
-
-def linha(p1, p2, pontos):
-    pygame.draw.line(screen, GREEN, (pontos[p1][0], pontos[p1][1]), (pontos[p2][0], pontos[p2][1]), 1)
-
-
-####################################
-
-
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("AlphaCube")
-
+# start the main loop
+# tick the clock to limit the frame rate to 30 frames per second
 clock = pygame.time.Clock()
-while True:
-    clock.tick(80)
+running = True
+while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                quit()
-
-    ############# Matriz de rotação #############
-    # referencia: https://en.wikipedia.org/wiki/Rotation_matrix
-    R_z = np.matrix([
-        [math.cos(angulo), -math.sin(angulo), 0],
-        [math.sin(angulo), math.cos(angulo), 0],
-        [0, 0, 1]
-    ])
-
-    R_y = np.matrix([
-        [math.cos(angulo), 0, math.sin(angulo)],
-        [0, 1, 0],
-        [-math.sin(angulo), 0, math.cos(angulo)]
-    ])
-
-    R_x = np.matrix([
-        [1, 0, 0],
-        [0, math.cos(angulo), -math.sin(angulo)],
-        [0, math.sin(angulo), math.cos(angulo)]
-    ])
-
+            running = False
+    
+    # fill the screen with black
     screen.fill((0, 0, 0))
+    
+    # update the rotation angle and draw the cube
+    angle += 0.01
+    draw_cube(angle)
+    
+    # update the display
+    pygame.display.flip()
 
-    angulo += 0.01
-
-    i = 0
-    for ponto in pontos:
-        Xd = R_z @ ponto.T
-        Xd = R_y @ Xd
-        Xd = R_x @ Xd
-        para2d = matriz_de_projecao @ Xd
-
-        pontos_2d[i] = [int(para2d[0, 0] * 100 + 400), int(para2d[1, 0] * 100 + 300)]
-
-        pygame.draw.circle(screen, RED, (int(para2d[0, 0] * 100 + 400), int(para2d[1, 0] * 100 + 300)), 5)
-        i += 1
-
-    linha(0, 1, pontos_2d)
-    linha(1, 2, pontos_2d)
-    linha(2, 3, pontos_2d)
-    linha(3, 0, pontos_2d)
-    linha(4, 5, pontos_2d)
-    linha(5, 6, pontos_2d)
-    linha(6, 7, pontos_2d)
-    linha(7, 4, pontos_2d)
-    linha(0, 4, pontos_2d)
-    linha(1, 5, pontos_2d)
-    linha(2, 6, pontos_2d)
-    linha(3, 7, pontos_2d)
-
-    pygame.display.update()
+# quit Pygame properly
+pygame.quit()
